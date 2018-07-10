332c59dc228f689142e08dac1b25452280726dda
peccu <peccul@gmail.com>
2018-07-10 10:27:45 +0900

    add template

---
+++ HillCharts.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+  name: '',
+  props: [
+  ],
+  components: {
+  },
+  computed: {
+  },
+  data () {
+  return {
+  }
+  },
+  watch: {
+  },
+  beforeCreate () {
+  },
+  created () {
+  },
+  beforeMount () {
+  },
+  mounted () {
+  },
+  beforeUpdate () {
+  },
+  updated () {
+  },
+  beforeDestroy () {
+  },
+  destroyed () {
+  },
+  methods: {
+  }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
